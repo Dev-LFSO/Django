c3b9fbf4b9cbffe524a53806ca13da1ae747c996
luix-091 <luiz.filipe.ctt@gmail.com>
2024-04-14 12:10:35 -0300

    v0.1

--- LocalDeEstudo/base/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Sala, Topico, Mensagem
-
-admin.site.register(Sala)
-admin.site.register(Topico)
-admin.site.register(Mensagem)
---
+++ SocialMedia/users/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from django.db.models import Q
+
+
+
+# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email_user = request.POST.get('email')
+        password = request.POST.get('password')
+        user_code = request.POST.get('code')
+
+        try:
+            user = User.objects.get(Q(email=email_user))
+        except User.DoesNotExist:
+            user = User.objects.create_user(username=username, email=email_user, password=password)
+            login(request, user)
+            return redirect('home')
+        return render(request, 'users/register.html', {'error': 'User already exists'})
+        
+        
+    return render(request, 'users/register.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        try:
+            user = User.objects.get(Q(email=request.POST.get('email')))
+            password = request.POST.get('password')
+
+            user = authenticate(username=user.username, password=password)
+            if user:
+                login(request, user)
+                if request.user.is_authenticated:
+                    return redirect('home')
+            return render(request, 'users/login.html', {'error': 'Invalid password'})
+        except User.DoesNotExist:
+            return render(request, 'users/login.html', {'error': 'Invalid credentials'})
+    return render(request, 'users/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
