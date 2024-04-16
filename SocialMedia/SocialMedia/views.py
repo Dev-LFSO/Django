d8c3e349e339c326f63cb31b72e6216da6780716
luix-091 <luiz.filipe.ctt@gmail.com>
2024-04-16 00:29:22 -0300

    v0.2.1

--- SocialMedia/SocialMedia/views.py
+++ SocialMedia/SocialMedia/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from datetime import datetime
+from GoogleNews import GoogleNews
 
 # Create your views here.
 
 def home(request):
-    #news = GoogleNews(lang='pt')
-    #news.enableException(True)
-    #news_list = news.get_news('APPLE')
-    #print(news_list)
-    return render(request, 'home.html')
+    googlenews = GoogleNews(period='1d')
+    today = datetime.today().strftime('%d/%m/%Y')
+    googlenews.set_time_range('01/01/2024', today)
+    googlenews.search('world')
+    news_list = googlenews.result()[0:9]
+    for news in news_list:
+        try:
+            news['datetime'] = news['datetime'].strftime('%H:%M - %d/%m/%Y')
+        except AttributeError:
+            pass
+    return render(request, 'home.html' , {'news_list':news_list})
 
