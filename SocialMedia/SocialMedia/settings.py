1152b787d2fbc107b9f51f8637450f44cfabfccc
Dev-LFSO <luiz.filipe.ctt@gmail.com>
2026-02-05 12:27:39 -0300

    v0.4.4

--- SocialMedia/SocialMedia/settings.py
+++ SocialMedia/SocialMedia/settings.py
@@ -24,9 +24,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kdbg(1qf$6411i0fb(vfx0e%hj$90t)q^-zmfngacno4*21u5s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.onrender.com',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://django-u8s3.onrender.com',
+]
+
 
 
 # Application definition
